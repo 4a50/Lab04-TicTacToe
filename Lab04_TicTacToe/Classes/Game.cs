5ec680483ace16eafb489461670cd144d3a3fe62
4a50 <jpjones22@gmail.com>
2021-01-14 18:05:07 -0800

    Completed Validating player choice, and updating matrix.

--- Lab04_TicTacToe/Classes/Game.cs
+++ Lab04_TicTacToe/Classes/Game.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 // get player input
 // switch cases
@@ -11,35 +9,45 @@ using System.Text;
 
 namespace Lab04_TicTacToe
 {
-	public class Game
-	{
-		public Player PlayerOne { get; set; }
-		public Player PlayerTwo { get; set; }
-		public Player Winner { get; set; }
-		public Board Board { get; set; }
+    public class Game
+    {
+        public Player PlayerOne { get; set; }
+        public Player PlayerTwo { get; set; }
+        public Player Winner { get; set; }
+        public Board Board { get; set; }
 
 
-		/// <summary>
-		/// Require 2 players and a board to start a game. 
-		/// </summary>
-		/// <param name="p1">Player 1</param>
-		/// <param name="p2">Player 2</param>
-		public Game(Player p1, Player p2)
-		{
-			PlayerOne = p1;
-			PlayerTwo = p2;
-			Board = new Board();
-		}
+        /// <summary>
+        /// Require 2 players and a board to start a game. 
+        /// </summary>
+        /// <param name="p1">Player 1</param>
+        /// <param name="p2">Player 2</param>
+        public Game(Player p1, Player p2)
+        {
+            PlayerOne = p1;
+            PlayerTwo = p2;
+            Board = new Board();
+        }
 
-		/// <summary>
-		/// Activate the Play of the game
-		/// </summary>
-		/// <returns>Winner</returns>
-		public Player Play()
-		{
+        /// <summary>
+        /// Activate the Play of the game
+        /// </summary>
+        /// <returns>Winner</returns>
+        public Player Play()
+        {
 
-			//TODO: Complete this method and utilize the rest of the class structure to play the game.
+            //TODO: Complete this method and utilize the rest of the class structure to play the game.
+            bool isValid = false;
+            //Determine Whose Turn
+            do
+            {
+                Console.WriteLine($"{player.Name} pick you spot:");
+                string userInput = Console.Readline();
+                isValid = GetValidPosition(userInput);
+                if (!isValid) Console.WriteLine("Invalid Entry.  Try again");
+            } while (!isValid);
 
+            UpdateMatrix(userInput);
 
 
             /*
@@ -56,79 +64,126 @@ namespace Lab04_TicTacToe
 
             Use any and all pre-existing methods in this program to help construct the method logic. 
              */
-		}
+        }
+
+        public int[] GetLocationOnBoard(string userInput)
+        {
+
+            switch (userInput)
+            {
+                case "1":
+                    return new int[] { 0, 0 };
+                    break;
+                case "2":
+                    return new int[] { 0, 1 };
+                    break;
+                case "3":
+                    return new int[] { 0, 2 };
+                    break;
+                case "4":
+                    return new int[] { 1, 0 };
+                    break;
+                case "5":
+                    return new int[] { 1, 1 };
+                    break;
+                case "6":
+                    return new int[] { 1, 2 };
+                    break;
+                case "7":
+                    return new int[] { 2, 0 };
+                    break;
+                case "8":
+                    return new int[] { 2, 1 };
+                    break;
+                case "9":
+                    return new int[] { 2, 2 };
+                    break;
+
+            }
+            public string UpdateMatrix(int[] loc, string playerMark)
+            {
+                string testLocation = Gameboard[loc[0], loc[1]];
+                Gameboard[loc[0], loc[1]] = playerMark;
+                return testLocation;
+
+            }
+            public bool GetValidPosition(string userInput)
+            {
+                int number = 0;
+                if (!int.TryParse(userInput, int number)) return false;
+                if (number < 1 || number > 10) return false;
+                return true;
+            }
+            /// <summary>
+            /// Check if winner exists
+            /// </summary>
+            /// <param name="board">current state of the board</param>
+            /// <returns>if winner exists</returns>
+            public bool CheckForWinner(Board board)
+            {
+                int[][] winners = new int[][]
+                {
+                new[] {1,2,3},
+                new[] {4,5,6},
+                new[] {7,8,9},
+
+                new[] {1,4,7},
+                new[] {2,5,8},
+                new[] {3,6,9},
+
+                new[] {1,5,9},
+                new[] {3,5,7}
+                };
+
+                // Given all the winning conditions, Determine the winning logic. 
+                for (int i = 0; i < winners.Length; i++)
+                {
+                    Position p1 = Player.PositionForNumber(winners[i][0]);
+                    Position p2 = Player.PositionForNumber(winners[i][1]);
+                    Position p3 = Player.PositionForNumber(winners[i][2]);
+
+                    string a = Board.GameBoard[p1.Row, p1.Column];
+                    string b = Board.GameBoard[p2.Row, p2.Column];
+                    string c = Board.GameBoard[p3.Row, p3.Column];
+
+                    // TODO:  Determine a winner has been reached. 
+                    // return true if a winner has been reached. 
+
+                }
+
+                return false;
+            }
 
 
-		/// <summary>
-		/// Check if winner exists
-		/// </summary>
-		/// <param name="board">current state of the board</param>
-		/// <returns>if winner exists</returns>
-		public bool CheckForWinner(Board board)
-		{
-			int[][] winners = new int[][]
-			{
-				new[] {1,2,3},
-				new[] {4,5,6},
-				new[] {7,8,9},
+            /// <summary>
+            /// Determine next player
+            /// </summary>
+            /// <returns>next player</returns>
+            public Player NextPlayer()
+            {
+                return (PlayerOne.IsTurn) ? PlayerOne : PlayerTwo;
+            }
 
-				new[] {1,4,7},
-				new[] {2,5,8},
-				new[] {3,6,9},
+            /// <summary>
+            /// End one players turn and activate the other
+            /// </summary>
+            public void SwitchPlayer()
+            {
+                if (PlayerOne.IsTurn)
+                {
 
-				new[] {1,5,9},
-				new[] {3,5,7}
-			};
-
-			// Given all the winning conditions, Determine the winning logic. 
-			for (int i = 0; i < winners.Length; i++)
-			{
-				Position p1 = Player.PositionForNumber(winners[i][0]);
-				Position p2 = Player.PositionForNumber(winners[i][1]);
-				Position p3 = Player.PositionForNumber(winners[i][2]);
-
-				string a = Board.GameBoard[p1.Row, p1.Column];
-				string b = Board.GameBoard[p2.Row, p2.Column];
-				string c = Board.GameBoard[p3.Row, p3.Column];
-
-				// TODO:  Determine a winner has been reached. 
-				// return true if a winner has been reached. 
-			
-			}
-
-			return false;
-		}
+                    PlayerOne.IsTurn = false;
 
 
-		/// <summary>
-		/// Determine next player
-		/// </summary>
-		/// <returns>next player</returns>
-		public Player NextPlayer()
-		{
-			return (PlayerOne.IsTurn) ? PlayerOne : PlayerTwo;
-		}
-
-		/// <summary>
-		/// End one players turn and activate the other
-		/// </summary>
-		public void SwitchPlayer()
-		{
-			if (PlayerOne.IsTurn)
-			{
-              
-				PlayerOne.IsTurn = false;
-
-              
-				PlayerTwo.IsTurn = true;
-			}
-			else
-			{
-				PlayerOne.IsTurn = true;
-				PlayerTwo.IsTurn = false;
-			}
-		}
+                    PlayerTwo.IsTurn = true;
+                }
+                else
+                {
+                    PlayerOne.IsTurn = true;
+                    PlayerTwo.IsTurn = false;
+                }
+            }
 
 
-	}
-}
+        }
+    }
--- Lab04_TicTacToe/Program.cs
+++ Lab04_TicTacToe/Program.cs
@@ -17,10 +17,10 @@ namespace Lab04_TicTacToe
             Player p1 = GetPlayers(1);
             Player p2 = GetPlayers(2);
             Game ticTacEry = new Game(p1, p2);
-            
+
 
             // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
-            
+
         }
         static Player GetPlayers(int playerNum)
         {
